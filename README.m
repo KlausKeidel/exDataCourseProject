faa8528b9f8f0cba9dcd21ceca84e752637d7908
Klaus Keidel <keidel.k@gmail.com>
2015-12-12 23:00:38 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,5 +1,5 @@
-## Files for the ExData_Plotting 1
-========================================================
+### Files for the ExData_Plots 1
+### ===============================
 
 Files based on the instructions of the JHB - Course-Project 1
 
